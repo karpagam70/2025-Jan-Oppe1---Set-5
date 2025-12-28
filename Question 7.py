571c33278b4dec1e2969553a8dfe1c8aae3e7dfa
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 20:05:02 +0530

    Create Question 7.py

---
+++ Question 7.py
@@ -0,0 +1,37 @@
+'''
+Pattern Printing - Hexagon
+Given an integer n as input, print a hexagon pattern with '@' having a side length of n.
+
+Examples
+
+Input
+
+2
+Output
+
+ @@
+@@@@
+ @@
+Input
+
+3
+Output
+
+  @@@
+ @@@@@
+@@@@@@@
+ @@@@@
+  @@@
+Input
+
+4
+Output
+
+   @@@@
+  @@@@@@
+ @@@@@@@@
+@@@@@@@@@@
+ @@@@@@@@
+  @@@@@@
+   @@@@
+'''
