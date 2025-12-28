7b5ffa95d2cd0ce6792cc7e9c1defecba7c76ee0
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 20:01:30 +0530

    Create Question 3.py

---
+++ Question 3.py
@@ -0,0 +1,17 @@
+'''
+Check If a String has No Vowels in Even Indices
+Write a function has_no_vowels_in_even_indices that takes a string s as input and returns True if there are no vowels in the even indices of the string, otherwise returns False.
+
+NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+Examples
+
+>>> has_no_vowels_in_even_indices("babebibobu") # The vowels are present in odd indices
+True
+>>> has_no_vowels_in_even_indices("abcde") # a is present in even index 0
+False
+>>> has_no_vowels_in_even_indices("test") # only vowel e is present in odd index 1
+True 
+>>> has_no_vowels_in_even_indices("why") # no vowels
+True 
+'''
